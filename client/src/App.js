9f1e706c2e5dadb58ed04072cce97657be279655
Thomas Brockington <xtombrock1989@gmail.com>
2023-01-18 22:02:45 +0000

    about

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import React from "react";
-// import Header from './components/header/Header'
-// import Nav from './components/nav/Nav'
-// import About from './components/about/About'
-// import Experience from './components/experience/Experience'
-// import Services from './components/services/Services'
-// import Portfolio from './components/portfolio/Portfolio'
-// import Testimonials from './components/testimonials/Testimonials'
-// import Contact from './components/contact/Contact'
-// import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
 function App() {
   return (
     <>
-    Hello World 2
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </>
   );
 }
