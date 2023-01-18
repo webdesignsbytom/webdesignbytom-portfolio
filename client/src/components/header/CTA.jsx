b51ffa3fda3feb6d7c7d8ed1af62a292534a837e
Thomas Brockington <xtombrock1989@gmail.com>
2023-01-18 22:06:09 +0000

    working setup

--- client/src/components/header/CTA.jsx
+++ client/src/components/header/CTA.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import CV from '../../assets/mock-cv.pdf'
+// import CV from '../../assets/mock-cv.pdf'
+let x = 1
 
 function CTA() {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
+        <a href={x} download className='btn'>Download CV</a>
         <a href='#contact' className='btn btn-primary'>Lets Chat</a>
     </div>
   )
