af8187aaf4ac0975884d9e95b8df5f4668781a0e
Thomas Brockington <xtombrock1989@gmail.com>
2023-01-27 11:33:48 +0000

    new react icon

--- client/src/components/orbitAnimation/OrbitAnimation.jsx
+++ client/src/components/orbitAnimation/OrbitAnimation.jsx
@@ -6,13 +6,15 @@ import ReactIcon from '../../assets/images/react.svg'
 import NodeIcon from '../../assets/images/nodejs.svg'
 import CSSIcon from '../../assets/images/css3.svg'
 import ExpressJS from '../../assets/images/expressjs.svg'
+import ReactJSIcon from '../../assets/images/reactjs-icon.svg';
+
 
 function OrbitAnimation() {
   return (
     <div className='orbit__container'>
       <div className='boundry__container'>
         <div className='circle'></div>
-        <div class='satellite satellite1'><img src={ReactIcon} alt="react icon" /></div>
+        <div class='satellite satellite1'><img src={ReactJSIcon} alt="react icon" /></div>
         <div class='satellite satellite2'><img src={GitHubIcon} alt="github icon" /></div>
         <div class='satellite satellite3'><img src={HTML5Icon} alt="html5 icon" /></div>
         <div class='satellite satellite4'><img src={NodeIcon} alt="node js icon" /></div>
