94e2b724144f1510aed8191cc4decff67127229a
Thomas Brockington <xtombrock1989@gmail.com>
2023-01-27 00:06:59 +0000

    centered

--- client/src/components/orbitAnimation/OrbitAnimation.jsx
+++ client/src/components/orbitAnimation/OrbitAnimation.jsx
@@ -4,13 +4,13 @@ import './orbitAnimation.css';
 function OrbitAnimation() {
   return (
     <div className='orbit__container'>
-      <div className='outer__circle'>
+      <div className='boundry__container'>
         <div className='circle'></div>
-      </div>
-      <div class='satellite1'>🛰️</div>
-      <div class='satellite2'>🚀</div>
+        <div class='satellite1'>🛰️</div>
+        {/* <div class='satellite2'>🚀</div>
       <div class='satellite3'>👽</div>
-      <div class='satellite4'>☄️</div>
+      <div class='satellite4'>☄️</div> */}
+      </div>
     </div>
   );
 }
