e548c1a51c5ef05897a0a494c671cccb0c495360
Thomas Brockington <xtombrock1989@gmail.com>
2023-02-21 16:49:54 +0000

    save

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import './footer.css'
-import { FaFacebookF } from 'react-icons/fa'
-import { BsLinkedin } from 'react-icons/bs'
+// Icons
+import { BsLinkedin, BsInstagram } from 'react-icons/bs'
 import { FaGithub } from 'react-icons/fa'
-
+import { FiTwitter } from 'react-icons/fi'
+// Styles
+import './footer.css'
 
 
 function Footer() {
@@ -16,21 +17,21 @@ function Footer() {
         <li><a href='#about'>About</a></li>
         <li><a href='#experience'>Experience</a></li>
         <li><a href='#services'>Services</a></li>
-        <li><a href='#Portfolio'>Portfolio</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
         <li><a href='#testimonials'>Testimonials</a></li>
         <li><a href='#contact'>Contact</a></li>
       </ul>
 
       <div className="footer__socials">
-        {/* TODO: ADD LINKS */}
-        <a target='_blank' rel='noreferrer' href='https://facebook.com'><FaFacebookF/></a>
+        <a target='_blank' rel='noreferrer' href='https://www.instagram.com/webdesignsbytom/'><BsInstagram/></a>
         <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/tom-brockington-b011b8230/'><BsLinkedin/></a>
         <a target='_blank' rel='noreferrer' href='https://github.com/webdesignbytom'><FaGithub/></a>
+        <a target='_blank' rel='noreferrer' href='https://twitter.com/webdesignsbytom'><FiTwitter/></a>
       </div>
 
       <div className="footer__copyright">
         <small>
-          Build by webdesignbytom. All rights reserved ©. 
+          Built by webdesignsbytom. All rights reserved 2023 ©. 
         </small>
       </div>
     </footer>
